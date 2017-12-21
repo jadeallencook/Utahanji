61ac20d8f0baaf1cbace62b8b9353de9b02e7896
Jade Allen Cook <jcook@deseretnews.com>
2017-12-21 14:15:19 -0700

    📐 Set global style up

--- components/main-menu/main-menu.js
+++ components/main-menu/main-menu.js
@@ -1,7 +1,6 @@
 const mainMenu = {
   build: function() {
-    //set background color
-      document.body.style.backgroundColor = 'black';
+    
   },
   init: function() {
     console.log('main menu started!');
