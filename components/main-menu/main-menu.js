370cc01e1b3102ac87a7e44d988f7d85e86e65a5
Clayton Cook <clayton.cook@muthschools.com>
2017-12-21 15:47:53 -0500

    added hearts

--- components/main-menu/main-menu.js
+++ components/main-menu/main-menu.js
@@ -1,6 +1,7 @@
 const mainMenu = {
   build: function() {
-
+    //set background color
+      document.body.style.backgroundColor = 'black';
   },
   init: function() {
     console.log('main menu started!');
